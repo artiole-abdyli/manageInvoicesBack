75537a9edc27aefc9647bced12f54495dc84bcf2
Artiole Abdyli <artiole.abdyli@mesh-web.com>
2025-05-10 21:47:02 +0200

    reservations table

---
+++ database/migrations/2025_05_10_194310_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('contact_id')->constrained('contacts')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+
+            $table->string('date');
+            $table->integer('number_of_dresses');
+            $table->string('returning_date');
+            $table->string('payment');
+            $table->string('extra_requirement')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
