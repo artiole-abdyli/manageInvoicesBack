8c4163f9f4451d43c32a97b18abdc3d264681b8e
Artiole Abdyli <artiole.abdyli@mesh-web.com>
2025-05-10 21:50:40 +0200

    reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Reservation extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+
+    protected $fillable = [
+        'contact_id',
+        'product_id',
+        'date',
+        'number_of_dresses',
+        'returning_date',
+        'payment',
+        'extra_requirement'
+    ];
+}
