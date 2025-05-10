36521119c670c5ee4ebaec3fb3d389efeabfdd56
Artiole Abdyli <artiole.abdyli@mesh-web.com>
2025-05-10 13:18:06 +0200

    contacts crud

---
+++ app/Models/Contact.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Contact extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'city',
+        'country',
+        'phone_number'
+    ];
+}
---
+++ app/Services/ContactService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ContactService
+{
+    protected $contact;
+    protected $user;
+
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    public function registerCompany(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->role = "user";
+
+        $user->save();
+        $company = new Company();
+        $company->company_name = $request->input("company_name");
+        $company->max_number_of_employees = $request->input("max_number_of_employees");
+        $company->save();
+        return response()->json('company is created successfully');
+    }
+}
