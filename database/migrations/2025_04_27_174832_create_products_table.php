4d468a6c51c8cae57585392a846d65cfd60bc58f
Artiole Abdyli <artiole.abdyli@mesh-web.com>
2025-05-16 16:25:30 +0200

    products changes

--- database/migrations/2025_04_27_174832_create_products_table.php
+++ database/migrations/2025_04_27_174832_create_products_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('description')->nullable();
             $table->decimal('price', 10, 2)->nullable();
+            $table->string('image')->nullable();
+            $table->string('status')->nullable();
+
             $table->integer('number_of_reservation')->nullable();
             $table->timestamps();
         });
