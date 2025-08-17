28079eca50897f2bd7eaea4f20f659b300c15e0e
Artiole Abdyli <artiole.abdyli@mesh-web.com>
2025-08-17 13:17:53 +0200

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Spatie\Permission\Traits\HasRoles;
-use Jenssegers\Mongodb\Eloquent\Model;
 
 
 class User extends Authenticatable implements JWTSubject
--- config/database.php
+++ config/database.php
@@ -42,17 +42,7 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'artiole.dc2.wbits.cloud'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'your_db_name'),
-            'username' => env('DB_USERNAME', 'admin'),
-            'password' => env('DB_PASSWORD', 'your_secure_password'),
-            'options'  => [
-                'database' => 'admin', // authentication database
-            ],
-        ],
+
 
         'mysql' => [
             'driver' => 'mysql',
